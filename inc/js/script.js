0dad2e96caa1997a3a1d5c40f86daf7f3b8b948a
mdashikul798 <37145914+mdashikul798@users.noreply.github.com>
2020-07-01 14:38:41 +0600

    First Web page
    This is a demo page

---
+++ inc/js/script.js
@@ -0,0 +1,59 @@
+// Get the modal
+var modal = document.getElementById('id01');
+function initMap(){
+      // Map options
+      var options = {
+        zoom:8,
+        center:{lat:23.873751,lng:90.396454}
+      }
+
+      // New map
+      var map = new google.maps.Map(document.getElementById('map'), options);
+
+      // Listen for click on map
+      google.maps.event.addListener(map, 'click', function(event){
+        // Add marker
+        addMarker({coords:event.latLng});
+      });
+
+      // Array of markers
+      var markers = [
+        {
+          coords:{lat:23.873751,lng:90.396454},
+          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          content:'<h1>Lynn MA</h1>'
+        }
+      ];
+
+      // Loop through markers
+      for(var i = 0;i < markers.length;i++){
+        // Add marker
+        addMarker(markers[i]);
+      }
+
+      // Add Marker Function
+      function addMarker(props){
+        var marker = new google.maps.Marker({
+          position:props.coords,
+          map:map,
+          //icon:props.iconImage
+        });
+
+        // Check for customicon
+        if(props.iconImage){
+          // Set icon image
+          marker.setIcon(props.iconImage);
+        }
+
+        // Check content
+        if(props.content){
+          var infoWindow = new google.maps.InfoWindow({
+            content:props.content
+          });
+
+          marker.addListener('click', function(){
+            infoWindow.open(map, marker);
+          });
+        }
+      }
+    }
